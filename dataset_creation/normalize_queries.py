378f8906702e96c744a9d9bf3c4e09688c6cf65a
Ben Eyal <ben.eyal.89@gmail.com>
2023-08-16 21:21:18 +0300

    Add query cleaning script, update dataset

---
+++ dataset_creation/normalize_queries.py
@@ -0,0 +1,30 @@
+import argparse
+import json
+from pathlib import Path
+
+import regex as re
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=Path)
+    parser.add_argument("-o", "--output", type=Path)
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        qpls = json.load(f)
+
+    result = []
+    for ex in qpls:
+        query = ex["query"]
+        query = re.sub(r"OPTION \(.*\)", "", query)
+        query = re.sub(r"WITH \(FORCESCAN\)", "", query)
+        ex["clean_query"] = query.strip()
+        result.append(ex)
+
+    with open(args.output, "w") as f:
+        json.dump(result, f, indent=2)
+
+
+if __name__ == "__main__":
+    main()

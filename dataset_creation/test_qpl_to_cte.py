39cdf09860ad7002174a4a6b7ce33c87d6db9624
Amir Bachar <amirbachar@gmail.com>
2023-09-25 21:57:43 +0300

    Change test to use actual DB (CTE is not deterministic)

--- dataset_creation/test_qpl_to_cte.py
+++ dataset_creation/test_qpl_to_cte.py
@@ -1,18 +1,51 @@
 import unittest
 
 from qpl_to_cte import *
+from sqlalchemy import create_engine
+from run_queries import get_results
 
 class TestFlatQplToCte(unittest.TestCase):
 
-    def test_group_by_all_columns(self):
+    def setUp(self):
+        self.engine = create_engine(
+            "mssql+pyodbc://SA:Passw0rd!@0.0.0.0/spider?driver=ODBC+Driver+17+for+SQL+Server"
+        )
+
+    def tearDown(self):
+        self.engine.dispose()
+
+    def test_group_by_no_output(self):
         qpl = [
             "#1 = Scan Table [ singer ] Output [ Name , Country , Age ]",
             "#2 = Aggregate [ #1 ] GroupBy [ Name , Country , Age ] Output [ countstar AS Count_Star ]"
         ]
         db_id = "concert_singer"
-        res = str(flat_qpl_to_cte(qpl, db_id))
-        expected = \
-'WITH Scan_1 AS ( SELECT Name, Country, Age FROM concert_singer.singer ), ' + \
- 'Aggregate_2 AS ( SELECT Name, Country, Age, COUNT(*) AS Count_Star FROM Scan_1 GROUP BY Name , ' + \
- 'Country , Age ) SELECT * FROM Aggregate_2'
+        cte = str(flat_qpl_to_cte(qpl, db_id))
+        res = get_results(cte, self.engine)
+        expected = [
+            {'age': 52, 'count_star': 1, 'country': 'Netherlands', 'name': 'Joe Sharp'},
+            {'age': 43, 'count_star': 1, 'country': 'France', 'name': 'John Nizinik'},
+            {'age': 29, 'count_star': 1, 'country': 'France', 'name': 'Justin Brown'},
+            {'age': 41, 'count_star': 1, 'country': 'France', 'name': 'Rose White'},
+            {'age': 32, 'count_star': 1, 'country': 'United States', 'name': 'Timbaland'},
+            {'age': 25, 'count_star': 1, 'country': 'France', 'name': 'Tribal King'}
+        ]
+        self.assertEqual(res, expected, "Group by columns should be returned in CTE")
+
+    def test_group_by_few_columns(self):
+        qpl = [
+            "#1 = Scan Table [ singer ] Output [ Name , Country , Age ]",
+            "#2 = Aggregate [ #1 ] GroupBy [ Name , Country , Age ] Output [ Name , countstar AS Count_Star ]"
+        ]
+        db_id = "concert_singer"
+        cte = str(flat_qpl_to_cte(qpl, db_id))
+        res = get_results(cte, self.engine)
+        expected = [
+            {'age': 52, 'count_star': 1, 'country': 'Netherlands', 'name': 'Joe Sharp'},
+            {'age': 43, 'count_star': 1, 'country': 'France', 'name': 'John Nizinik'},
+            {'age': 29, 'count_star': 1, 'country': 'France', 'name': 'Justin Brown'},
+            {'age': 41, 'count_star': 1, 'country': 'France', 'name': 'Rose White'},
+            {'age': 32, 'count_star': 1, 'country': 'United States', 'name': 'Timbaland'},
+            {'age': 25, 'count_star': 1, 'country': 'France', 'name': 'Tribal King'}
+        ]
         self.assertEqual(res, expected, "Group by columns should be returned in CTE")
